5b00eb10163db9fc506954982c9783c2455261e2
N. Idate <in.my.challenge@gmail.com>
2018-12-28 11:18:02 +0900

    Add validations to models/user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname,
+    presence: true,
+    length: { maximum: 20 }
+  validates :email,
+    presence: true,
+    format: { with: /\A[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*[a-zA-Z]+\z/ }
+  validates :password,
+    presence: true,
+    format: { with: /\A(?=.*[^\d])+/ },
+    length: { in: 6..128 }
 end
