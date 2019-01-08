6b4439022929e3637f4d4512b69488506231a48d
N. Idate <in.my.challenge@gmail.com>
2019-01-08 19:45:58 +0900

    Add default_locale

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,9 @@ module FreemarketSample
       g.helper false
       g.test_framework false
     end
+    config.time_zone = 'Tokyo'
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
