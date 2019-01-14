a1a35494c126f01ea3b02856d68b7fadc2ae4575
N. Idate <in.my.challenge@gmail.com>
2019-01-14 22:47:59 +0900

    Add temporary variable

--- app/controllers/mypage/mypage_controller.rb
+++ app/controllers/mypage/mypage_controller.rb
@@ -2,6 +2,12 @@ class Mypage::MypageController < ApplicationController
   before_action :move_to_root
 
   def index
+    @user = User.find(current_user.id)
+    # TODO:仮のデータ修正
+    @notice = [{id: 1, text: 'お知らせ：お得なキャンペーン始まりました！詳細はこちら'}]
+    @todo = []
+    @trading_items = []
+    @purchased_items = []
   end
 
   private
