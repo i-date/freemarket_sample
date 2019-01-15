0e74b43c1e998f921b4837914291565689cb2f38
N. Idate <in.my.challenge@gmail.com>
2019-01-15 09:37:01 +0900

    Modify javascripts/mypage.js

--- app/assets/javascripts/mypage.js
+++ app/assets/javascripts/mypage.js
@@ -5,14 +5,14 @@ $(document).on('turbolinks:load', function () {
   left_tab.on('click', function () {
     $(this).parent().children('.tag-box__tab--right').removeClass('tab-active');
     $(this).addClass('tab-active');
-    $(this).parent().siblings('.tag-box .tag-box__list').hide();
-    $(this).parent().siblings('.tag-box .tag-box__list:nth-child(2)').show();
+    $(this).parent().siblings('.tag-box__list').hide();
+    $(this).parent().siblings('.tag-box__list:nth-child(2)').show();
   })
 
   right_tab.on('click', function () {
     $(this).parent().children('.tag-box__tab--left').removeClass('tab-active');
     $(this).addClass('tab-active');
-    $(this).parent().siblings('.tag-box .tag-box__list').hide();
-    $(this).parent().siblings('.tag-box .tag-box__list:last-child').show();
+    $(this).parent().siblings('.tag-box__list').hide();
+    $(this).parent().siblings('.tag-box__list:last-child').show();
   })
 });
