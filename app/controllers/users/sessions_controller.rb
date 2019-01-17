dee580300d14f30093b8f83803da81f85ecd134d
N. Idate <in.my.challenge@gmail.com>
2019-01-17 16:59:37 +0900

    Remove unnecessary lines

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,21 +1,5 @@
 class Users::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
   before_action :check_captcha, only: [:create]
-
-  # GET /resource/sign_in
-  # def new
-  #   super
-  # end
-
-  # POST /resource/sign_in
-  # def create
-  #   super
-  # end
-
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
 
   private
 
@@ -33,9 +17,4 @@ class Users::SessionsController < Devise::SessionsController
       resource.errors[:email].pop
     end
   end
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
 end
