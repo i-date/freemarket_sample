552f62c6b23f2e4064e5b4d3231df4ce66b86bab
N. Idate <in.my.challenge@gmail.com>
2019-01-17 12:46:18 +0900

    Add devise sessions and modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users, skip: :all,
     controllers: {
       omniauth_callbacks: 'users/omniauth_callbacks',
-      registrations:      'users/registrations'
+      registrations:      'users/registrations',
+      sessions:      'users/sessions'
     }
 
   get '/', to: redirect('/jp')
@@ -16,16 +17,16 @@ Rails.application.routes.draw do
     # TODO:仮のURIを修正
     devise_scope :user do
       # session
-      get    'login',                 to: 'devise/sessions#new',         as: :new_user_session
-      post   'login',                 to: 'devise/sessions#create',      as: :user_session
-      delete 'logout',                to: 'devise/sessions#destroy',     as: :destroy_user_session
+      get    'login',                 to: 'users/sessions#new',          as: :new_user_session
+      post   'login',                 to: 'users/sessions#create',       as: :user_session
+      delete 'logout',                to: 'users/sessions#destroy',      as: :destroy_user_session
       # registration
       get    'signup',                to: 'users/registrations#index',   as: :start_user_registration
-      get    'signup/registration',   to: 'devise/registrations#new',    as: :new_user_registration
-      patch  'users',                 to: 'devise/registrations#update', as: :user_registration
-      put    'users',                 to: 'devise/registrations#update'
-      delete 'users',                 to: 'devise/registrations#destroy'
-      post   'signup/registration',   to: 'devise/registrations#create', as: :create_user_registration
+      get    'signup/registration',   to: 'users/registrations#new',     as: :new_user_registration
+      patch  'users',                 to: 'users/registrations#update',  as: :user_registration
+      put    'users',                 to: 'users/registrations#update'
+      delete 'users',                 to: 'users/registrations#destroy'
+      post   'signup/registration',   to: 'users/registrations#create',  as: :create_user_registration
       # password
       get    'password/reset/start/', to: 'devise/passwords#new',        as: :new_user_password
       get    'password/edit',         to: 'devise/passwords#edit',       as: :edit_user_password
