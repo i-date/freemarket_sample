f03dfeff7dc6a5e468498738721e76b7a1a23171
N. Idate <in.my.challenge@gmail.com>
2019-01-17 17:55:25 +0900

    Add color variable

--- app/assets/javascripts/devise.js
+++ app/assets/javascripts/devise.js
@@ -5,28 +5,30 @@ $(document).on('turbolinks:load', function () {
   var registration_error_field = registration_form.siblings('.field_with_errors').children('input');
   var omniauth_form = $('.omniauth .field .error');
   var omniauth_error_field = omniauth_form.siblings('input');
+  var red_border = '#ea352d';
+  var blue_border = '#0099e8';
 
   if (session_form.length) {
-    session_error_field.css('border-color', '#ea352d');
+    session_error_field.css('border-color', red_border);
     session_error_field.on('click', function () {
-      session_error_field.css('border-color', '#ea352d');
-      $(this).css('border-color', '#0099e8');
+      session_error_field.css('border-color', red_border);
+      $(this).css('border-color', blue_border);
     });
   };
 
   if (registration_form.length) {
-    registration_error_field.css('border-color', '#ea352d');
+    registration_error_field.css('border-color', red_border);
     registration_error_field.on('click', function () {
-      registration_error_field.css('border-color', '#ea352d');
-      $(this).css('border-color', '#0099e8');
+      registration_error_field.css('border-color', red_border);
+      $(this).css('border-color', blue_border);
     })
   };
 
   if (omniauth_form.length) {
-    omniauth_error_field.css('border-color', '#ea352d');
+    omniauth_error_field.css('border-color', red_border);
     omniauth_error_field.on('click', function () {
-      omniauth_error_field.css('border-color', '#ea352d');
-      $(this).css('border-color', '#0099e8');
+      omniauth_error_field.css('border-color', red_border);
+      $(this).css('border-color', blue_border);
     })
   };
 });
