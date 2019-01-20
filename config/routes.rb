775c3cbb12f4e8b5bde05fa1370bf01c2b6f0a57
N. Idate <in.my.challenge@gmail.com>
2019-01-20 17:53:22 +0900

    Add routing

--- config/routes.rb
+++ config/routes.rb
@@ -14,8 +14,10 @@ Rails.application.routes.draw do
     get 'mypage', to: 'mypage/mypage#index', as: :mypage_top
     get 'logout', to: 'mypage/logout#index', as: :mypage_logout
     namespace :mypage do
-      get   'profile', to: 'profile#edit',   as: :profile
-      patch 'profile', to: 'profile#update', as: :profile_update
+      get   'profile',        to: 'profile#edit',          as: :profile
+      patch 'profile',        to: 'profile#update',        as: :profile_update
+      get   'identification', to: 'identification#edit',   as: :identification
+      patch 'identification', to: 'identification#update', as: :identification_update
     end
 
     # TODO:仮のURIを修正
