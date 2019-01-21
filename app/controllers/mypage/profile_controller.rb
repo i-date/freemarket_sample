2c5303bce3e5fae2ae2f3534ad9b5c5c08324b7d
N. Idate <in.my.challenge@gmail.com>
2019-01-21 12:29:37 +0900

    modify variable in profile_controller.rb

--- app/controllers/mypage/profile_controller.rb
+++ app/controllers/mypage/profile_controller.rb
@@ -1,19 +1,20 @@
 class Mypage::ProfileController < ApplicationController
 
   def edit
-    @user_detail = UserDetail.new
-    @user = User.find(current_user.id)
-    @profile = Profile.find_by(user_id: current_user.id).present? ? Profile.find_by(user_id: currnet_user.id) : Profile.new
+    @profile = current_user.profile
   end
 
   def update
+    @profile = current_user.profile
+    @profile.nickname = profile_params[:nickname]
+    @profile.body = profile_params[:body]
     # TODO: レコードの更新用コードを記述
     render :edit
   end
 
   private
 
-  def user_detail_params
-    params.require(:user_detail).permit(:nickname, :body, :last_name, :first_name, :last_name_kana, :first_name_kana, :birth_year, :birth_month, :birth_day, :phone_number, :zipcode, :prefecture, :city, :block, :building)
+  def profile_params
+    params.require(:profile).permit(:nickname, :body)
   end
 end
