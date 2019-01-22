142481076c5e7f06d05161773b1d4fd83dfd4b69
N. Idate <in.my.challenge@gmail.com>
2019-01-22 17:28:54 +0900

    Add items variable

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
 
   def index
+    @items = Item.order("updated_at DESC").limit(4)
     @categories = Category.eager_load(children: :children).where(parent_id: 0)
   end
 end
