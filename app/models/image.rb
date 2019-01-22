9b215aba7714cbf9a29081ca8139d60a0cfffa22
N. Idate <in.my.challenge@gmail.com>
2019-01-22 17:16:15 +0900

    Add association

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,2 +1,6 @@
 class Image < ApplicationRecord
+  belongs_to :item
+
+  validates :name, presence: true
+  validates :item_id, presence: true
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,7 @@ class Item < ApplicationRecord
   belongs_to :category
   belongs_to :user
   belongs_to :size
+  has_many :images
 
   enum condition: {
     新品・未使用: 1, 未使用に近い: 2, 目立った傷や汚れなし: 3, やや傷や汚れあり: 4, 傷や汚れあり: 5, 全面的に状態が悪い: 6
