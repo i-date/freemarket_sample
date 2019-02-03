4ef9bffa91fc40b820b4d91dd33dde0b26b366c7
N. Idate <in.my.challenge@gmail.com>
2019-02-03 17:27:53 +0900

    Add actions

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,17 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new]
+  layout 'devise', only: [:new]
 
   def index
     @items = Item.order("updated_at DESC").limit(4)
+  end
+
+  def new
+    @item = Item.new
+    @item.images.build
+    @sizes = Size.all
+  end
+
+  def create
   end
 end
