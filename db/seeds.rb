a86238809540cb832e99e99820518b578aa013b6
N. Idate <in.my.challenge@gmail.com>
2019-02-14 16:55:08 +0900

    Add status seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,24 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-require "csv"
+require 'csv'
 
-# CSV.foreach('db/category.csv') do |record|
-#   Category.create(name: record[0], parent_id: record[1])
-# end
+# ユーザー
+User.create(nickname: 'test', email: 'test@test.com', password: 'test12345678', password_confirmation: 'test12345678')
 
-# CSV.foreach('db/brand.csv') do |record|
-#   Brand.create(name: record[0], brand_group_id: record[1])
-# end
+# カテゴリー
+CSV.foreach('db/category.csv') do |record|
+  Category.create(name: record[0], parent_id: record[1])
+end
 
-# CSV.foreach('db/brand_group.csv') do |record|
-#   BrandGroup.create(name: record[0])
-# end
+# ブランド
+CSV.foreach('db/brand.csv') do |record|
+  Brand.create(name: record[0], brand_group_id: record[1])
+end
 
-# CSV.foreach('db/size.csv') do |record|
-#   Size.create(name: record[0])
-# end
+# ブランドグループ
+CSV.foreach('db/brand_group.csv') do |record|
+  BrandGroup.create(name: record[0])
+end
 
-# CSV.foreach('db/item.csv') do |record|
-#   Item.create(name: record[0], price: record[1], description: record[2], condition: record[3], shipping_fee: record[4], shipping_from: record[5], days_before_shipping: record[6], shipping_method: record[7], status: record[8], brand: record[9], category_id: record[10], user_id: record[11], size_id: record[12])
-# end
+# サイズ
+CSV.foreach('db/size.csv') do |record|
+  Size.create(name: record[0])
+end
+
+# 商品ステータス
+Status.create([{ name: '販売中'}, { name: '取引中' }, { name: '販売済み' }, { name: '出品停止' }])
+
+# 商品
+CSV.foreach('db/item.csv') do |record|
+  Item.create(name: record[0], price: record[1], description: record[2], condition: record[3], shipping_fee: record[4], shipping_from: record[5], days_before_shipping: record[6], shipping_method: record[7], status_id: record[8], brand: record[9], category_id: record[10], user_id: record[11], size_id: record[12])
+end
