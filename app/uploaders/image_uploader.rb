2beb4d8d4b60f2e0ec2ae87ca41ea3743abd5302
N. Idate <in.my.challenge@gmail.com>
2019-02-17 09:56:16 +0900

    modify image_uploader.rb for secure file name

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -42,6 +42,12 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
   def filename
-    original_filename if original_filename
+    "#{secure_token}.#{file.extension}" if original_filename
+  end
+
+  protected
+  def secure_token
+    var = :"@#{mounted_as}_secure_token"
+    model.instance_variable_get(var) or model.instance_variable_set(var, SecureRandom.uuid)
   end
 end
