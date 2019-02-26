9ad8cc09e10d6feb64ed9dabf901b66d76124a93
N. Idate <in.my.challenge@gmail.com>
2019-02-26 14:47:11 +0900

    Modify search action and add methods for ransack

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -62,8 +62,19 @@ class ItemsController < ApplicationController
   end
 
   def search
-    @search_result = @query.result.sort_update_desc.includes(:images)
-    @all_items = Item.sort_update_desc.includes(:images) if @search_result.blank?
+    # ransack用変数設定
+    initilize_ransack_variable
+    initilize_ransack_symbol
+    # クエリー設定
+    q = set_query_params
+    # 検索結果取得
+    if q.length != 1
+      @query = Item.ransack(q)
+      @search_result = @query.result.includes(:images)
+    end
+    @new_items = Item.sort_update_desc.limit(24).includes(:images)
+    # 再検索のため、クエリー初期化
+    @query = Item.ransack({})
   end
 
   private
@@ -86,6 +97,23 @@ class ItemsController < ApplicationController
 
   def image_params
     params.require(:images).require(:name).permit(params[:images][:name].keys) if params[:images].present?
+  end
+
+  def query_params
+    params.require(:q).permit(
+      :name_or_description_or_brand_or_category_name_or_category_parent_name_or_category_grandparent_name_cont,
+      :sorts,
+      :size_id_eq,
+      :price_gteq,
+      :price_lteq,
+      :brand_cont_any,
+      :category_grandparent_id_eq,
+      :category_parent_id_eq,
+      category_id_eq_any: [],
+      condition_eq_any: [],
+      shipping_fee_eq_any: [],
+      status_id_eq_any: [],
+    ) if params[:q].present?
   end
 
   def set_item
@@ -125,6 +153,82 @@ class ItemsController < ApplicationController
       item[key.to_sym] = val
     end
     return item
+  end
+
+  def initilize_ransack_variable
+    # 詳細検索用インスタンス変数
+    @sort_list = Item.sort_select_list
+    @size_list = Size.all
+    @price_list = Item.price_select_list
+    @condition_list = Item.condition_check_list
+    @fee_list = Item.fee_check_list
+    @status_list = Item.status_check_list
+  end
+
+  def initilize_ransack_symbol
+    # 詳細検索対象カラム指定、このメソッド内のみでransackに必要なシンボルを操作決定
+    @keyword_sym = :name_or_description_or_brand_or_category_name_or_category_parent_name_or_category_grandparent_name_cont
+    @sort_sym = :sorts
+    @category_sym = :category_id_eq_any
+    @category_parent_sym = :category_parent_id_eq
+    @category_grandparent_sym = :category_grandparent_id_eq
+    @brand_sym = :brand_cont_any
+    @size_sym = :size_id_eq
+    @price_gteq_sym = :price_gteq
+    @price_lteq_sym = :price_lteq
+    @condition_sym = :condition_eq_any
+    @fee_sym = :shipping_fee_eq_any
+    @status_sym = :status_id_eq_any
+  end
+
+  def set_query_params
+    # ransack用変数設定
+    initilize_ransack_symbol
+    @q = {}
+
+    # 検索キーワード
+    @query_keyword = query_params[@keyword_sym].present? ? query_params[@keyword_sym] : '検索結果'
+    @keywords = query_params[@keyword_sym]
+    # 以下複数キーワード（AND）検索用
+    if @keywords.present?
+      @q[:groupings] = []
+      @keywords.split(/[\s|　]+/).each_with_index do |word, i|
+        @q[:groupings][i] = { @keyword_sym => word }
+      end
+    end
+    # topカテゴリー
+    @q[@category_grandparent_sym] = query_params[@category_grandparent_sym] if query_params[@category_grandparent_sym].present?
+    # middleカテゴリー
+    @q[@category_parent_sym] = query_params[@category_parent_sym] if query_params[@category_parent_sym].present?
+    # bottomカテゴリー
+    @q[@category_sym] = query_params[@category_sym] if query_params[@category_sym].present?
+    # ブランド
+    @q[@brand_sym] = query_params[@brand_sym].split(/[\s|　]+/) if query_params[@brand_sym].present?
+    # サイズ
+    @q[@size_sym] = query_params[@size_sym] if query_params[@size_sym].present?
+    # 最低価格
+    @q[@price_gteq_sym] = query_params[@price_gteq_sym] if query_params[@price_gteq_sym].present?
+    # 最高価格
+    @q[@price_lteq_sym] = query_params[@price_lteq_sym] if query_params[@price_lteq_sym].present?
+    # 商品状態
+    @q[@condition_sym] = query_params[@condition_sym] if query_params[@condition_sym].present?
+    # 配送料負担
+    @q[@fee_sym] = query_params[@fee_sym] if query_params[@fee_sym].present?
+    # 販売状況
+    @q[@status_sym] = query_params[@status_sym].join(',').split(',') if query_params[@status_sym].present?
+
+    # ソートを選択した場合
+    if query_params[@sort_sym].present?
+      @q = session[:search_q]
+      @q[@sort_sym] = query_params[@sort_sym]
+    end
+    # デフォルトソート設定
+    @q[@sort_sym] = 'updated_at DESC' if @q[@sort_sym].blank?
+
+    # ソート用にセッションに保存
+    session[:search_q] = @q
+
+    return @q
   end
 end
 
