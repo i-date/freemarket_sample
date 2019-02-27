f0bf670f560e2b1582d9613d083bf2d25afae0ff
N. Idate <in.my.challenge@gmail.com>
2019-02-27 13:09:51 +0900

    Add methods for credit card form

--- app/controllers/concerns/common.rb
+++ app/controllers/concerns/common.rb
@@ -6,4 +6,14 @@ module Common
   def move_to_root
     redirect_to root_path unless user_signed_in?
   end
+
+  def set_years
+    min_year = Time.new.year.to_s[2,2].to_i
+    max_year = min_year + 10
+    return years = [*(min_year..max_year)]
+  end
+
+  def set_months
+    return [*(1..12)].map { |num| '%02d' % num }
+  end
 end
