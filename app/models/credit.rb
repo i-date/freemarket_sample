14ec6b4bf98428f94b33e9dc555d0db410d1c32d
N. Idate <in.my.challenge@gmail.com>
2019-02-27 16:47:11 +0900

    Modify validations

--- app/models/credit.rb
+++ app/models/credit.rb
@@ -14,10 +14,10 @@ class Credit < ApplicationRecord
 
   validates :authorization_code,
     presence: true,
-    format: { with: /\A\d{14,16}/, allow_blank: true, message: "この番号は登録できません" }
+    format: { with: /\A\d{14,16}\z/, allow_blank: true, message: "この番号は登録できません" }
   validates :security_code,
     presence: true,
-    format: { with: /\A\d{3,4}/, allow_blank: true, message: "この番号は登録できません" }
+    format: { with: /\A\d{3,4}\z/, allow_blank: true, message: "この番号は登録できません" }
   validates :month,
     presence: { message: "選択して下さい" },
     format: { with: /0[1-9]|1[0-2]/, message: "無効な選択です" }
