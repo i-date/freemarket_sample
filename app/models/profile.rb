05d650e6c78404e7524fc8033ea949d2a80385f1
N. Idate <in.my.challenge@gmail.com>
2019-02-27 22:04:13 +0900

    Add instance methods

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -37,7 +37,6 @@ class Profile < ApplicationRecord
   validates :birth_day,
     presence: true
   validates :phone_number,
-    presence: true,
     length: { maximum: 11, message: "11文字で入力して下さい" },
     uniqueness: { message: "この電話番号は既に登録されています。" },
     format: { with: /\A0[789]0\d{8}/, allow_blank: true, message: "この電話番号は登録できません" }
@@ -50,4 +49,16 @@ class Profile < ApplicationRecord
     presence: true
   validates :block,
     presence: true
+
+  def set_hyphen
+    self.zipcode.to_s.insert(3, '-')
+  end
+
+  def set_address
+    self.prefecture_i18n + "\s" + self.city + "\s" + self.block + "\s" + self.building
+  end
+
+  def set_fullname
+    self.last_name + "\s" + self.first_name
+  end
 end
