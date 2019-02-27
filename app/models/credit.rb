79b6074d7a22de6b0b99b6078c940fff929d05b7
N. Idate <in.my.challenge@gmail.com>
2019-02-27 15:45:12 +0900

    Update credit.rb

--- app/models/credit.rb
+++ app/models/credit.rb
@@ -1,10 +1,16 @@
 class Credit < ApplicationRecord
   belongs_to :user
 
-  # :yearに対するバリデーション用正規表現作成
-  min_year = Time.new.year.to_s[2,2].to_i
-  max_year = min_year + 10
-  year_regex = [*(min_year..max_year)].join('|')
+  # :yearに対するバリデーション用正規表現作成にも使用、そのためvalidation前に以下のメソッドを記述
+  def self.get_years
+    min_year = Time.new.year.to_s[2,2].to_i
+    max_year = min_year + 10
+    return years = [*(min_year..max_year)]
+  end
+
+  def self.get_months
+    ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12']
+  end
 
   validates :authorization_code,
     presence: true,
@@ -17,17 +23,7 @@ class Credit < ApplicationRecord
     format: { with: /0[1-9]|1[0-2]/, message: "無効な選択です" }
   validates :year,
     presence: { message: "選択して下さい" },
-    format: { with: /#{year_regex}/, message: "無効な選択です" }
+    format: { with: /#{Credit.get_years.join('|')}/, message: "無効な選択です" }
   validates :user_id,
     presence: true
-
-  def self.get_years
-    min_year = Time.new.year.to_s[2,2].to_i
-    max_year = min_year + 10
-    return years = [*(min_year..max_year)]
-  end
-
-  def self.get_months
-    ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12']
-  end
 end
