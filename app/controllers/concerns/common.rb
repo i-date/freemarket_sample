839ce26c32f3c174ad15a85fa9a7e510cf445fcd
N. Idate <in.my.challenge@gmail.com>
2019-02-28 14:03:27 +0900

    Add method for changing string to boolean

--- app/controllers/concerns/common.rb
+++ app/controllers/concerns/common.rb
@@ -6,4 +6,12 @@ module Common
   def move_to_root
     redirect_to root_path unless user_signed_in?
   end
+
+  def to_boolean(string)
+    if string == 'true'
+      true
+    elsif string == 'false'
+      false
+    end
+  end
 end
