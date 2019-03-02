a6f195c5261b010657d032ba9a718ca86cc190f2
N. Idate <in.my.challenge@gmail.com>
2019-03-02 17:14:29 +0900

    Modify comment from English to Japanese

--- spec/controllers/transaction/buy_controller_spec.rb
+++ spec/controllers/transaction/buy_controller_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe Transaction::BuyController, type: :controller do
 
   describe 'GET #show' do
 
-    context "login" do
+    context "ログイン:" do
       before do
         @item = item
         @images = @item.images
@@ -22,36 +22,30 @@ RSpec.describe Transaction::BuyController, type: :controller do
         get :show, params: { id: @item[:id] }
       end
 
-      # @itemという変数が正しく定義されているか
-      it "assigns the requested item to @item" do
+      it "- @itemという変数が正しく定義" do
         expect(assigns(:item)).to eq(@item)
       end
 
-      # @imagesという変数が正しく定義されているか
-      it "assigns the requested images to @images" do
+      it "- @imagesという変数が正しく定義" do
         expect(assigns(:images)).to eq(@images)
       end
 
-      # @credit_cardという変数が正しく定義されているか
-      it "assigns the requested credit_card to @credit_card" do
+      it "- @credit_cardという変数が正しく定義" do
         expect(assigns(:credit_card)).to eq(@credit_card)
       end
 
-      # @profileという変数が正しく定義されているか
-      it "assigns the requested profile to @profile" do
+      it "- @profileという変数が正しく定義" do
         expect(assigns(:profile)).to eq(@profile)
       end
 
-      # 該当するビューが描画されているか
-      it "renders the :show template" do
+      it "- showテンプレートが描画" do
         expect(response).to render_template :show
       end
     end
 
-    context "not login" do
+    context "ログアウト:" do
 
-      # 意図したビューにリダイレクトしているか
-      it "redirects to new_user_session_path" do
+      it "- new_user_session_pathにリダイレクト" do
         get :show, params: { id: item[:id] }
         expect(response).to redirect_to(new_user_session_path)
       end
@@ -60,12 +54,12 @@ RSpec.describe Transaction::BuyController, type: :controller do
 
   describe 'POST #create' do
 
-    context "login" do
+    context "ログイン:" do
       before do
         login_user users.last
       end
 
-      context "can save" do
+      context "アクション成功:" do
         before do
           @item = item
           @credit_card = credit
@@ -74,38 +68,33 @@ RSpec.describe Transaction::BuyController, type: :controller do
           allow(Payjp::Charge).to receive(:create).and_return(PayjpMock.prepare_valid_charge)
         end
 
-        # クレジットカード情報の更新は成功か
-        it 'payjp_token of credit is successfully updated' do
+        it '- クレジットカード情報の更新成功' do
           post :create, params: { id: @item.id, payjp_token: 'tok_76e202b409f3da51a0706605ac81' }
           @credit_card.payjp_token = 'cus_121673955bd7aa144de5a8f6c262'
           expect(assigns(:credit_card)).to eq(@credit_card)
         end
 
-        # 取引相手の保存は成功か
-        it 'trading_partner' do
+        it '- 取引相手の保存成功' do
           expect{ post :create, params: { id: @item.id, payjp_token: 'tok_76e202b409f3da51a0706605ac81' } }.to change(TradingPartner, :count).by(1)
         end
 
-        # オーダーの保存は成功か
-        it 'order' do
+        it '- オーダーの保存成功' do
           expect{ post :create, params: { id: @item.id, payjp_token: 'tok_76e202b409f3da51a0706605ac81' } }.to change(Order, :count).by(1)
         end
 
-        # 商品ステータスの更新は成功か
-        it 'item' do
+        it '- 商品ステータスの更新成功' do
           post :create, params: { id: @item.id, payjp_token: 'tok_76e202b409f3da51a0706605ac81' }
           @item.status_id = 2
           expect(assigns(:item)).to eq(@item)
         end
 
-        # 意図したビューにリダイレクトしているか
-        it "redirects to root_path" do
+        it "- root_pathにリダイレクト" do
           post :create, params: { id: @item.id, payjp_token: 'tok_76e202b409f3da51a0706605ac81' }
           expect(response).to redirect_to(root_path)
         end
       end
 
-      context "can not save" do
+      context "アクション失敗" do
         before do
           @item = item
           @credit_card = credit
@@ -113,107 +102,98 @@ RSpec.describe Transaction::BuyController, type: :controller do
           login_user users.last
         end
 
-        context "with already sold item" do
+        context "販売中のアイテム以外の場合:" do
           before do
             @item.update(status_id: 2)
             allow(Payjp::Customer).to receive(:create).and_return(PayjpMock.prepare_valid_customer)
             allow(Payjp::Charge).to receive(:create).and_return(PayjpMock.prepare_valid_charge)
           end
 
-          # クレジットカード情報の更新は失敗か（payjpの顧客作成失敗）
-          it 'payjp_token of credit is not updated' do
+          it '- クレジットカード情報の更新失敗' do
             post :create, params: { id: @item.id, payjp_token: 'tok_76e202b409f3da51a0706605ac81' }
+            @credit_card.payjp_token = 'cus_121673955bd7aa144de5a8f6c262'
             expect(assigns(:credit_card)).to eq(@credit_card)
           end
 
-          # 取引相手の保存は失敗か
-          it 'trading_partner' do
+          it '- 取引相手の保存失敗' do
             expect{ post :create, params: { id: @item.id, payjp_token: 'tok_76e202b409f3da51a0706605ac81' } }.to change(TradingPartner, :count).by(0)
           end
 
-          # オーダーの保存は失敗か
-          it 'order' do
+          it '- オーダーの保存失敗' do
             expect{ post :create, params: { id: @item.id, payjp_token: 'tok_76e202b409f3da51a0706605ac81' } }.to change(Order, :count).by(0)
           end
 
-          # 商品ステータスの更新は失敗か
-          it 'item' do
+          it '- 商品ステータスの更新失敗' do
             post :create, params: { id: @item.id, payjp_token: 'tok_76e202b409f3da51a0706605ac81' }
+            @item.status_id = 2
             expect(assigns(:item)).to eq(@item)
           end
 
-          # 意図したビューにリダイレクトしているか
-          it "redirects to transaction_buy_path" do
+          it "- transaction_buy_pathにリダイレクト" do
             post :create, params: { id: @item.id, payjp_token: 'tok_76e202b409f3da51a0706605ac81' }
             expect(response).to redirect_to(transaction_buy_path(@item))
           end
         end
 
-        context "without correct response of payjp customer" do
+        context "payjp顧客登録レスポンスエラーの場合:" do
           before do
             allow(Payjp::Customer).to receive(:create).and_return(Payjp::CardError.new('', {}, 402))
             allow(Payjp::Charge).to receive(:create).and_return(PayjpMock.prepare_valid_charge)
           end
 
-          # クレジットカード情報の更新は失敗か（payjpの顧客作成失敗）
-          it 'payjp_token of credit is not updated' do
+          it '- クレジットカード情報の更新失敗' do
             post :create, params: { id: @item.id, payjp_token: 'tok_76e202b409f3da51a0706605ac81' }
+            @credit_card.payjp_token = 'cus_121673955bd7aa144de5a8f6c262'
             expect(assigns(:credit_card)).to eq(@credit_card)
           end
 
-          # 取引相手の保存は失敗か
-          it 'trading_partner' do
+          it '- 取引相手の保存失敗' do
             expect{ post :create, params: { id: @item.id, payjp_token: 'tok_76e202b409f3da51a0706605ac81' } }.to change(TradingPartner, :count).by(0)
           end
 
-          # オーダーの保存は失敗か
-          it 'order' do
+          it '- オーダーの保存失敗' do
             expect{ post :create, params: { id: @item.id, payjp_token: 'tok_76e202b409f3da51a0706605ac81' } }.to change(Order, :count).by(0)
           end
 
-          # 商品ステータスの更新は失敗か
-          it 'item' do
+          it '- 商品ステータスの更新失敗' do
             post :create, params: { id: @item.id, payjp_token: 'tok_76e202b409f3da51a0706605ac81' }
+            @item.status_id = 2
             expect(assigns(:item)).to eq(@item)
           end
 
-          # 意図したビューにリダイレクトしているか
-          it "redirects to transaction_buy_path" do
+          it "- transaction_buy_pathにリダイレクト" do
             post :create, params: { id: @item.id, payjp_token: 'tok_76e202b409f3da51a0706605ac81' }
             expect(response).to redirect_to(transaction_buy_path(@item))
           end
         end
 
-        context "without correct response of payjp charge" do
+        context "payjp支払いレスポンスエラーの場合:" do
           before do
             allow(Payjp::Customer).to receive(:create).and_return(PayjpMock.prepare_valid_customer)
             allow(Payjp::Charge).to receive(:create).and_raise(Payjp::CardError.new('', {}, 402))
           end
 
-          # クレジットカード情報の更新は失敗か（payjpの顧客作成失敗）
-          it 'payjp_token of credit is not updated' do
+          it '- クレジットカード情報の更新失敗' do
             post :create, params: { id: @item.id, payjp_token: 'tok_76e202b409f3da51a0706605ac81' }
+            @credit_card.payjp_token = 'cus_121673955bd7aa144de5a8f6c262'
             expect(assigns(:credit_card)).to eq(@credit_card)
           end
 
-          # 取引相手の保存は失敗か
-          it 'trading_partner' do
+          it '- 取引相手の保存失敗' do
             expect{ post :create, params: { id: @item.id, payjp_token: 'tok_76e202b409f3da51a0706605ac81' } }.to change(TradingPartner, :count).by(0)
           end
 
-          # オーダーの保存は失敗か
-          it 'order' do
+          it '- オーダーの保存失敗' do
             expect{ post :create, params: { id: @item.id, payjp_token: 'tok_76e202b409f3da51a0706605ac81' } }.to change(Order, :count).by(0)
           end
 
-          # 商品ステータスの更新は失敗か
-          it 'item' do
+          it '- 商品ステータスの更新失敗' do
             post :create, params: { id: @item.id, payjp_token: 'tok_76e202b409f3da51a0706605ac81' }
+            @item.status_id = 2
             expect(assigns(:item)).to eq(@item)
           end
 
-          # 意図したビューにリダイレクトしているか
-          it "redirects to transaction_buy_path" do
+          it "- transaction_buy_pathにリダイレクト" do
             post :create, params: { id: @item.id, payjp_token: 'tok_76e202b409f3da51a0706605ac81' }
             expect(response).to redirect_to(transaction_buy_path(@item))
           end
@@ -221,10 +201,9 @@ RSpec.describe Transaction::BuyController, type: :controller do
       end
     end
 
-    context "not login" do
+    context "ログアウト:" do
 
-      # 意図したビューにリダイレクトしているか
-      it "redirects to new_user_session_path" do
+      it "- new_user_session_pathにリダイレクト" do
         post :create
         expect(response).to redirect_to(new_user_session_path)
       end
