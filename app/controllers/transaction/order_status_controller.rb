035f4b67ab3913472c2b5062b4660c2a355b7cd1
N. Idate <in.my.challenge@gmail.com>
2019-03-03 15:51:35 +0900

    Remove unnecessary words

--- app/controllers/transaction/order_status_controller.rb
+++ app/controllers/transaction/order_status_controller.rb
@@ -1,5 +1,5 @@
 class Transaction::OrderStatusController < ApplicationController
-  before_action :authenticate_user!, only: [:show, :create]
+  before_action :authenticate_user!, only: :show
   layout 'devise'
 
   def show
